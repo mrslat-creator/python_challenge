eb8187fafa1cb23a233a84287bff56e766823a69
Altricia Latimer <altricia.latimer@gmail.com>
2021-03-25 21:59:11 -0400

    adding final lines of code for mainbank.py

--- PyBank/mainbank.py
+++ PyBank/mainbank.py
@@ -9,6 +9,8 @@ month_total = 0
 profit = 0
 sum_profit = 0
 sum_loss = 0
+totalpl = 0
+month_change = []
 
 with open(csv_filepath, newline="") as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
@@ -16,24 +18,21 @@ with open(csv_filepath, newline="") as csvfile:
     # print(len(spamreader))
     for row in spamreader:
         month_total += 1
-        
-# The total number of months included in the dataset
-end_date = datetime.datetime(2017, 2)
-start_date = datetime.datetime(2010, 1)
 
-num_months = (end_date.year - start_date.year) * 12 + (end_date.month -
+# The total number of months included in the dataset
+end_date = datetime.datetime(2017, 1, 2)
+start_date = datetime.datetime(2010, 1, 1)
+
+num_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
 
 print(num_months)
 
 #  The net total amount of "Profit/Losses" over the entire period
-for row in csv_filepath:
+for row in csv_filepath: 
     profit = int(row[1])
     if profit > 0:
-        sum_profit =sum_profit + profit
+        sum_profit = sum_profit + profit
     elif profit < 0:
-        sum_loss
-#  Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-
-#  The greatest increase in profits (date and amount) over the entire period
-
-#  The greatest decrease in losses (date and amount) over the entire period
+        sum_loss = sum_loss + profit
+    totalpl = sum_profit - sum_loss
+print(f"Total: {totalpl}")
